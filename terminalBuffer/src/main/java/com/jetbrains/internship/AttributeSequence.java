7b02400f633ec365339af568c26f3a4f47e8cd04
skopadav <skopadav@fel.cvut.cz>
2026-02-21 16:39:13 +0100

    basic Line operations - init, ger, clear. Modified AttributeSequence for public attributes to make my life easier

--- terminalBuffer/src/main/java/com/jetbrains/internship/AttributeSequence.java
+++ terminalBuffer/src/main/java/com/jetbrains/internship/AttributeSequence.java
@@ -4,7 +4,13 @@ package com.jetbrains.internship;
  * To keep mem footprint down sequences of same-style characters will share one CellAttribute class
  */
 public class AttributeSequence {
-    private int startCol;
-    private int length;
-    private CellAttribute attributes;
+    public int startCol; // inclusive
+    public int length; // includes start
+    public CellAttribute attributes;
+
+    public AttributeSequence(int startCol, int length, CellAttribute attributes) {
+        this.startCol = startCol;
+        this.length = length;
+        this.attributes = attributes;
+    }
 }
--- terminalBuffer/src/main/java/com/jetbrains/internship/Line.java
+++ terminalBuffer/src/main/java/com/jetbrains/internship/Line.java
@@ -3,6 +3,9 @@ package com.jetbrains.internship;
 import java.util.List;
 
 public final class Line {
+
+    public int width;
+
     /**
      * Unicode code point per cell.
      * Valid only if width != TRAILING.
@@ -12,7 +15,48 @@ public final class Line {
 
     public byte[] widths;
 
-    public List<AttributeSequence> attrRuns;
+    public List<AttributeSequence> attributeSequences;
 
     public boolean wrapped;
+
+    public Line(int width, CellAttribute defaultAttr) {
+        this.width = width;
+        this.codepoints = new int[width];
+        this.widths = new byte[width];
+        clear(defaultAttr);
+    }
+
+    public void clear(CellAttribute defaultAttr) {
+        for (int i = 0; i < width; i++) {
+            this.codepoints[i] = 0;
+            widths[i] = CellType.normal;
+        }
+        attributeSequences.clear();
+        attributeSequences.add(new AttributeSequence(0, width, defaultAttr));
+        wrapped = false;
+    }
+
+    public int getCodePoint(int col) {
+        if (widths[col] == CellType.trailing) {
+            // trailing cells do not contain anything as the character is stored in leading.
+            // On higher level (TerminalBuffer) API both cells will return the character unicode
+            return -1;
+        }
+        return codepoints[col];
+    }
+
+    public byte getCellWidth(int col) {
+        return widths[col];
+    }
+
+    // could be improved with binary search over linear, it is assumed the attributeSequences will
+    // not be long enough to matter
+    public CellAttribute getAttr(int col) {
+        for (AttributeSequence run : attributeSequences) {
+            if (col >= run.startCol && col < run.startCol + run.length) {
+                return run.attributes;
+            }
+        }
+        throw new IllegalStateException("No attributeSequence for column " + col);
+    }
 }
